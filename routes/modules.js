7df5f85e738308a393bf7045e4830d706efb220d
Rishab <rishabshirur26@gmail.com>
2023-05-03 18:12:34 -0400

    module and ann routing

--- routes/modules.js
+++ routes/modules.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 import { modulesData } from "../data/index.js";
 import validation from "../helper.js";
-router.route("/:courseId/module").get(async (req, res) => {
+router.route("/:courseId").get(async (req, res) => {
   try {
     if (!req.session.user) {
       return res.redirect("/login");
@@ -12,21 +12,25 @@ router.route("/:courseId/module").get(async (req, res) => {
     if (!moduleList) {
       throw "No modules found";
     }
+    if (req.session.user){
     if (req.session.user.role == "student") {
       res.render("modules/allModules", {
         moduleList: moduleList,
         student: true,
+        courseId:course
       });
     }
     if (req.session.user.role == "faculty") {
       res.render("modules/allModules", {
         moduleList: moduleList,
         faculty: true,
+        courseId:course
       });
     }
     if (req.session.user.role == "admin") {
-      res.render("modules/allModules", { moduleList: moduleList, admin: true });
+      res.render("modules/allModules", { moduleList: moduleList, admin: true ,courseId:course});
     }
+  }
   } catch (e) {
     res.status(500).json({ error: e });
   }
@@ -35,16 +39,25 @@ router.route("/:courseId/module").get(async (req, res) => {
 router
   .route("/:courseId/newModule")
   .get(async (req, res) => {
+    try {
+      req.params.courseId = validation.checkId(req.params.courseId, "Id URL Param");
+      
+    } catch (e) {
+      res.status(404).render("modules/allModules", { error: `${e}` });
+    }
     if (!req.session.user) {
       return res.redirect("/login");
     }
     if (req.session.user) {
       if (req.session.user.role == "student") {
-        return res.render("error");
+        return res.render('notallowed');
       }
-      if (req.session.user.role == "feculty") {
-        return res.render("modules/newModule");
-      }
+      // if (req.session.user.role == "feculty") {
+
+
+        let course=req.params.courseId;
+        return res.render("modules/newModule",{course:course});
+      // }
       // if(req.session.user.role=="admin"){
       //   return res.render('announcements/newAnnouncement');
       // }
@@ -71,7 +84,7 @@ router
       // anninfo.userId = validation.checkId(anninfo.userId, 'userId');
       // anninfo.description
     } catch (e) {
-      return res.status(400).render("modules/allModules", { error: e });
+      return res.status(400).render("modules/newModule",{course:course,error:`${e}`});
     }
 
     try {
@@ -92,20 +105,24 @@ router
       }
       return res.redirect("/module");
     } catch (e) {
-      res.status(400).render("modules/allmodules", { error: `${e}` });
+      res.status(400).render("error", { error: `${e}` });
     }
   });
 
-router.route("/module/:id").get(async (req, res) => {
+router.route("/:courseId/:id").get(async (req, res) => {
   try {
     req.params.id = validation.checkId(req.params.id, "Id URL Param");
+    req.params.courseId = validation.checkId(req.params.courseId, "Id URL Param");
+    
   } catch (e) {
-    res.status(404).render("modules/allModules", { error: `${e}` });
+    res.status(404).render("error", { error: `${e}` });
   }
   try {
     if (!req.session.user) {
       return res.redirect("/login");
     }
+    let course= req.params.courseId;
+
     const mod = await modulesData.get(req.params.id);
     let desc = mod.description;
     let user = mod.user;
@@ -116,13 +133,15 @@ router.route("/module/:id").get(async (req, res) => {
       user: user,
       description: desc,
       date: date,
+      course:course
+
     });
   } catch (e) {
     res.status(404).render("modules/allModules", { error: `${e}` });
   }
 });
 
-router.route("module/delete/:id").get(async (req, res) => {
+router.route("/:courseId/delete/:id").get(async (req, res) => {
   //code here for DELETE
   try {
     req.params.id = validation.checkId(req.params.id, "Id URL Param");
