028b4330b0a4386c1b2546179316dbe0c186c40b
Madhura Shinde <smadhura23@gmail.com>
2023-05-07 00:15:07 -0400

    fixed bugs in registerCheck

--- public/js/registerCheck.js
+++ public/js/registerCheck.js
@@ -62,15 +62,6 @@ let birthDateInput = document.getElementById("birthDateInput")
       throw `Password must contain at least one special character`;
     return strVal;
   };
-  
-//    const validRole = (strVal) => {
-//     if (!strVal) throw "Role can not be empty";
-//     if (typeof strVal !== "string") throw "Role must be a string";
-//     strVal = strVal.trim().toLowerCase();
-//     if (strVal !== "student" && strVal !== "faculty" && strVal !== "admin")
-//       throw `Error: Role can only be student, faculty or admin`;
-//     return strVal;
-//   };
 
    const checkBirthDateFormat = (strVal) => {
     if (!strVal) throw `Error: You must supply a string}!`;
@@ -91,7 +82,6 @@ let birthDateInput = document.getElementById("birthDateInput")
     const date = new Date();
     const currentYear = date.getFullYear();
     if (year > currentYear - 15) throw `Must be at least 15 years old`;
-    // if(year < 1900) throw `Relese date can not be lower than 1900`;
     if (month < 1 || month > 12) throw `Month must be between 1-12`;
     if (day < 1 || day > 31) throw `Day must be between 1-31`;
     if (month === 2 && day > 28)
@@ -99,12 +89,10 @@ let birthDateInput = document.getElementById("birthDateInput")
     if (month === 4 || month === 6 || month === 9 || month === 11) {
       if (day > 30) throw `Date can not be 31 for this month `;
     }
-    // if (!moment(strVal, "DD/MM/YYYY", true).isValid()) throw "not a valid date";
     return strVal;
   };
   
    const checkValidArray = (arr) => {
-    // if (!arr || !Array.isArray(arr) || arr.length === 0)
     if (!arr || !Array.isArray(arr)) throw "Array must has length > 0";
     let res = [];
     for (let i = 0; i < arr.length; i++) {
@@ -146,7 +134,6 @@ let birthDateInput = document.getElementById("birthDateInput")
       var gender = genderInput.value;
       var major = majorInput.value;
       var birthDate = birthDateInput.value;
-      console.log('here?');
       try {
         checkNameFormat(firstName);
         checkNameFormat(lastName);
@@ -157,7 +144,6 @@ let birthDateInput = document.getElementById("birthDateInput")
         checkValidMajor(major);
         checkBirthDateFormat(birthDate);
       } catch (e) {
-        console.log('catch');
         event.preventDefault();
         const message = typeof e === "string" ? e : e.message;
         errorDivs.textContent = message;
@@ -177,75 +163,5 @@ let birthDateInput = document.getElementById("birthDateInput")
         passwordInput.classList.add("inputClass");
 
       }
-  
-    //   try {
-    //     checkNameFormat(lastName);
-    //   } catch (e) {
-    //     event.preventDefault();
-    //     const message = typeof e === "string" ? e : e.message;
-    //     errorDivs.textContent = message;
-    //     errorDivs.hidden = false;
-    //     lastNameInput.value = lastName;
-    //     console.log(lastNameInput.value , lastName , 'madhuraisbad');
-    //     lastNameInput.classList.add("inputClass");
-    //   }
-    //   try {
-    //     checkEmailAddress(email);
-    //   } catch (e) {
-    //     event.preventDefault();
-    //     const message = typeof e === "string" ? e : e.message;
-    //     errorDivs.textContent = message;
-    //     errorDivs.hidden = false;
-    //     emailAddressInput.value = email;
-    //     emailAddressInput.classList.add("inputClass");
-    //   }
-    //   try {
-    //     checkValidMajor(major);
-    //   } catch (e) {
-    //     event.preventDefault();
-    //     const message = typeof e === "string" ? e : e.message;
-    //     errorDivs.textContent = message;
-    //     errorDivs.hidden = false;
-    //     majorInput.value = major;
-    //     majorInput.classList.add("inputClass");
-    //   }
-    //   try {
-    //     validPassword(password);
-    //   } catch (e) {
-    //     event.preventDefault();
-    //     const message = typeof e === "string" ? e : e.message;
-    //     errorDivs.textContent = message;
-    //     errorDivs.hidden = false;
-    //     passwordInput.classList.add("inputClass");
-    //   }
-    //   try {
-    //     validPassword(confirmPassword);
-    //   } catch (e) {
-    //     event.preventDefault();
-    //     const message = typeof e === "string" ? e : e.message;
-    //     errorDivs.textContent = message;
-    //     errorDivs.hidden = false;
-    //     confirmPasswordInput.classList.add("inputClass");
-    //   }
-    //   try {
-    //     validGender(gender);
-    //   } catch (e) {
-    //     event.preventDefault();
-    //     const message = typeof e === "string" ? e : e.message;
-    //     errorDivs.textContent = message;
-    //     errorDivs.hidden = false;
-    //     genderInput.value = gender;
-    //     genderInput.classList.add("inputClass");
-    //   }
-    //   try {
-    //     checkBirthDateFormat(birthDate);
-    //   } catch (e) {
-    //     event.preventDefault();
-    //     const message = typeof e === "string" ? e : e.message;
-    //     errorDivs.textContent = message;
-    //     errorDivs.hidden = false;
-    //     birthDateInput.value = birthDate;
-    //     birthDateInput.classList.add("inputClass");
-    //   }
     });
   }
